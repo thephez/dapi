dc1742de6661748758a3ca824280d8f6959eb8f2
thephez <thephez@users.noreply.github.com>
2019-06-19 13:44:48 -0400

    Config - Modify travis config and deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -26,14 +26,14 @@ fi
 
 IMAGE_NAME="dashpay/dapi"
 
-# 1. build image:
-docker build -t "${IMAGE_NAME}:latest" \
-             -t "${IMAGE_NAME}:${VERSION}" \
-             .
-
-# Login to Docker Hub
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-
-# Push images to the registry
-docker push "${IMAGE_NAME}:latest"
-docker push "${IMAGE_NAME}:${VERSION}"
+## 1. build image:
+#docker build -t "${IMAGE_NAME}:latest" \
+#             -t "${IMAGE_NAME}:${VERSION}" \
+#             .
+#
+## Login to Docker Hub
+#echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+#
+## Push images to the registry
+#docker push "${IMAGE_NAME}:latest"
+#docker push "${IMAGE_NAME}:${VERSION}"
